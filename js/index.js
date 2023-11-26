7057b6e8c1845aa677226f3830a7e7acf94f11f3
Brett C <brcannon@bellsouth.net>
2023-11-26 15:27:16 -0600

    updates to the index.js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,29 @@
 const apiKey = '51a65cbbdb2901662f04a3352d4fcb48'
-let city_name = 'hattiesburg'
+let bigNameEl = document.getElementById("single-day-city")
+let bigIconEl = document.getElementById("single-icon")
+let bigTempEl = document.getElementById("single-temp")
+let bigWindEl = document.getElementById("single-wind")
+let bigHumidityEl = document.getElementById("single-humidity")
+let bigDateEl = document.getElementById("single-date")
 
 
 
 // gets location
+function getCity() {
+    const newName = document.getElementById('searchForm')
+    const cityName = document.getElementById('single-day-city')
+    const city = newName.value.trim();
+    cityName.innerHTML = city
 
+    getWeather(city)
+
+}
 
 // gets weather for one location
 
-function getWeather() {
-    const weatehr_city_url = `https://api.openweathermap.org/geo/1.0/direct?q=${city_name}&limit=1&appid=${apiKey}`
-    fetch(weatehr_city_url)
+function getWeather(city) {
+    const weather_city_url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
+    fetch(weather_city_url)
         .then(
             function (response) {
                 return response.json();
@@ -26,26 +39,45 @@ function getWeather() {
                     })
                     .then(
                         function (weathterData) {
+                            
                             let name = weathterData.name
                             let wind = weathterData.wind
                             let description = weathterData.weather[0].description;
                             let icon = weathterData.weather[0].icon;
                             let humidity = weathterData.main.humidity;
+                            let currentDate = dayjs().format(`MM/DD/YYYY`)
                             
                             // console.log("Weather Data:", weatherData);
                             console.log("Name:", name)
-                            console.log("Wind:", wind);
+                            console.log("Wind:", wind.speed);
                             console.log("Description:", description);
                             console.log("Icon:", icon);
                             console.log("Humidity:", humidity);
+                           
+                            bigNameEl.innerHTML = name;
+                            bigWindEl.innerHTML(`Wind: ${wind.speed} mph `);
+                            bigIconEl.src = `https://openweathermap.org/img/w/${icon}.png`
+                            bigHumidityEl.innerHTML = `Humidity: ${humidity}%`
+                            bigDateEl.innerHTML = currentDate;
+                            console.log("Date:", currentDate);
+                            console.log("Updated HTML elements:", bigNameEl, bigWindEl, bigIconEl, bigHumidityEl, bigDateEl);
                         })
                     
                     }
         )
 
+    
+
 }
+  
+
+
+
+ 
+const searchButton = document.getElementById('searchForm-btn');
+    searchButton.addEventListener('click', function(){
+    getCity()
+});
 
 getWeather();
-
-
 // gets 5 day forcast
