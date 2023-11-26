9062e44c02ebb77464c3ee7a8e37309eb635e3d9
Brett C <brcannon@bellsouth.net>
2023-11-26 11:29:16 -0600

    able to get weather from api

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const apiKey = '51a65cbbdb2901662f04a3352d4fcb48'
-
+let city_name = 'hattiesburg'
 
 
 
@@ -9,27 +9,43 @@ const apiKey = '51a65cbbdb2901662f04a3352d4fcb48'
 // gets weather for one location
 
 function getWeather() {
-    const weatehr_city_url = `api.openweathermap.org/data/2.5/forecast?lat=${city_name}&appid=${apiKey}`
+    const weatehr_city_url = `https://api.openweathermap.org/geo/1.0/direct?q=${city_name}&limit=1&appid=${apiKey}`
     fetch(weatehr_city_url)
         .then(
             function (response) {
                 return response.json();
             })
-        .then(
-            function (data) {
-                let lat = data.lat;
-                let lon = data.lon;
-                const weather_lat_lon_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-                fetch(weather_lat_lon_url)
+        .then(function (data) {
+                let lat = data[0].lat;
+                let lon = data[0].lon;
+
+                const single_day_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+                fetch(single_day_url)
+                    .then(function (response) {
+                        return response.json();
+                    })
                     .then(
-                        function (response) {
-                            return response.json();
+                        function (weathterData) {
+                            let name = weathterData.name
+                            let wind = weathterData.wind
+                            let description = weathterData.weather[0].description;
+                            let icon = weathterData.weather[0].icon;
+                            let humidity = weathterData.main.humidity;
+                            
+                            // console.log("Weather Data:", weatherData);
+                            console.log("Name:", name)
+                            console.log("Wind:", wind);
+                            console.log("Description:", description);
+                            console.log("Icon:", icon);
+                            console.log("Humidity:", humidity);
                         })
-            }
+                    
+                    }
         )
 
 }
 
+getWeather();
 
 
 // gets 5 day forcast
